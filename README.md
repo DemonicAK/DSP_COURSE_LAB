169caf7cc1763e294a092d2bbe4997570581ffe8
arijit kar <demonicak07@gmail.com>
2023-08-21 11:26:05 +0530

    21/8

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Digital Signal Processing  lab work
 
 I have taken this subject in 5th sem of my Btech Degree
+
+exp1 -- plot discrete time signals
---
+++ exp_1.m
@@ -0,0 +1,34 @@
+% sin
+
+t=0:0.1:2*pi;
+y=sin(t);
+subplot(5,1,1);
+stem (t, y);
+
+
+
+t = -2 : 0.2 : 2;
+% impluse
+
+impulse = t==0;
+subplot(5,1,2);
+stem(t,impulse);
+
+% unit step
+unitstep = t>=0;
+subplot(5,1,3);
+stem(t,unitstep);
+
+% ramp
+ramp = t.*unitstep;
+subplot(5,1,4);
+stem(t,ramp);
+
+% exponential
+
+t = -3 : 0.3 : 3;
+expo=exp(t);
+subplot(5,1,5);
+stem(t,expo);
+
+
